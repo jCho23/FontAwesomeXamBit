9907faa3b4a6187e408e255cbaf3aca3afe94d14
June <junecho.ucb@gmail.com>
2018-02-12 12:32:03 -0800

    created FontAwesomebit

---
+++ FontAwesomeXamBit/Pages/FontAwesomeBit.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace FontAwesomeXamBit.Pages
+{
+    public partial class FontAwesomeBit : ContentPage
+    {
+        public FontAwesomeBit()
+        {
+            InitializeComponent();
+        }
+    }
+}
