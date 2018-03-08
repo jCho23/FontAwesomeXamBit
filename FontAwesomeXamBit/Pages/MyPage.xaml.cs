462ffb7fd019381a6195bf8dcca559bf3d066a22
June <junecho.ucb@gmail.com>
2018-03-08 15:33:13 -0800

    CleanUp

---
+++ FontAwesomeXamBit/Pages/MyPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿Fusing System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace FontAwesomeXamBit.Pages
+{
+    public partial class MyPage : ContentPage
+    {
+        public MyPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
